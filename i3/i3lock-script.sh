1e975128c8a9355a9f1a6a0e38d3eaebfeba94c8
Tiago Espinha <tiago@espinha.nl>
2017-06-17 22:44:57 +0200

    Updates

--- i3/i3lock-script.sh
+++ i3/i3lock-script.sh
@@ -14,7 +14,7 @@ BLURTYPE="0x5"
 #BLURTYPE="2x8" # 2.90s
 #BLURTYPE="2x3" # 2.92s
 
-import -window root png:- | convert - +level 20%,80%,0.6 -colorspace gray -fill "#DDDDFF" -tint 100 -blur $BLURTYPE -font Inconsolata -pointsize 26 -fill white -gravity center -annotate +0+200 'Please type your password' png:- > $IMAGE
+import -silent -window root png:- | convert - +level 20%,80%,0.6 -colorspace gray -fill "#DDDDFF" -tint 100 -blur $BLURTYPE -font Inconsolata -pointsize 26 -fill white -gravity center -annotate +0+200 'Please type your password' png:- > $IMAGE
 i3lock -i $IMAGE
 rm $IMAGE
 
